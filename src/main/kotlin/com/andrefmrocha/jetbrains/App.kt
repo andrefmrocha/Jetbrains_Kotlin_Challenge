aa35d4e2d28d1c8b6f149ec8c9bc874e0eef8355
Andre Rocha <andrefmrocha@live.com.pt>
2020-01-03 12:58:14 +0000

    Added example file for main process

--- src/main/kotlin/com/andrefmrocha/jetbrains/App.kt
+++ src/main/kotlin/com/andrefmrocha/jetbrains/App.kt
@@ -1,8 +1,14 @@
 @file:JvmName("App")
+
 package com.andrefmrocha.jetbrains
+
 import com.andrefmrocha.jetbrains.generation.JsonGenerator
 import java.io.File
 
 fun main() {
-    JsonGenerator(File("file.json")).build("Test", "com.andrefmrocha.meias", "src/main/kotlin")
+    JsonGenerator(File("src/main/resources/file.json")).build(
+        "Test",
+        "com.andrefmrocha.test",
+        "src/main/kotlin"
+    )
 }
